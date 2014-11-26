c481bb2d76ba0635b3862f2814f3cb80ad48b810
DeonHua <deonhbci@gmail.com>
2014-11-25 23:23:32 -0500

    got some stuff here

--- static/script.js
+++ static/script.js
@@ -88,22 +88,20 @@ app.controller('MainCtrl', function($scope, $http){
     };
 
 });
-
-app.directive("mathjaxBind", function() {
-    return {
-        restrict: "A",
-        controller: ["$scope", "$element", "$attrs", function($scope, $element, $attrs) {
-            $scope.$watch($attrs.mathjaxBind, function(value) {
-                var $script = angular.element("<script type='math/tex'>")
-                    .html(value == undefined ? "" : value);
-                $element.html("");
-                $element.append($script);
-                MathJax.Hub.Queue(["Reprocess", MathJax.Hub, $element[0]]);
+app.directive('mathjax',function(){
+	return {
+		restrict: 'EA',
+		link: function(scope, element, attrs) {
+			scope.$watch(attrs.ngModel, function () {
+				MathJax.Hub.Queue(['Typeset',MathJax.Hub,element.get(0)]);
             });
-        }]
-    };
+		}
+	};
 });
 
-function MyCtrl($scope, $element) {
-    $scope.expression = "\\frac{5}{4} \\div \\frac{1}{6}";
+function ExampleCtrl($scope){
+    $scope.changeModel = function(){
+         $scope.mjx = '\sum_{i=0}^\infty i^2';
+         console.log($scope.mjx);
+    }
 }
