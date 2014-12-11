898fedd310c49256ee7d8f722be59f09b6047b7f
DeonHua <deonhbci@gmail.com>
2014-12-11 18:49:09 -0500

    Fixed MathJax display.

--- app.py
+++ app.py
@@ -16,20 +16,13 @@ def hello():
 def about():
     return render_template("about.html")
 
-@app.route("/getTime/")
-def getTime():
-    figured = logic.getTime()
-    app.logger.debug(figured)
-    time = json.dumps(figured)
-    app.logger.debug(time)
-    return time
-
 @app.route("/getReducedMatrix/")
 def getMatrix():
     entries = [[3,4], [1,2]]
     matrix = Matrix(entries)
-    app.logger.debug(latex(matrix, mode = "equation", itex = True))
-    return latex(matrix, mode = "equation", itex = True)
+    matrix = matrix.rref()[0]
+    app.logger.debug(latex(matrix, mode="equation", itex = True))
+    return latex(matrix, mode="equation", itex = True)
 
 if __name__ == ("__main__"):
     app.run(debug=True,host='0.0.0.0',port=int(os.environ.get("PORT", 5000)))
--- static/script.js
+++ static/script.js
@@ -2,8 +2,15 @@
  * Created by Owner on 11/21/2014.
  */
 
+MathJax.Hub.Config({
+  jax: ["input/TeX","output/HTML-CSS","output/NativeMML"],
+  tex2jax:{
+    displayMath: [ ['$$','$$'], ["\\[","\\]"] ]
+  }
+});
+
 //Declare module
-var app = angular.module('FirstYear', ['ui.bootstrap']);
+var app = angular.module('LinAlgToolkit', ['ui.bootstrap']);
 
 app.config(function($interpolateProvider) {
   $interpolateProvider.startSymbol('{[{');
@@ -13,95 +20,22 @@ app.config(function($interpolateProvider) {
 
 app.controller('MainCtrl', function($scope, $http){
 
-    $scope.time = null;
-    $scope.finalMolarMass = null;
-    $scope.originalFormula = null;
     $scope.isError = null;
     $scope.matrix = null;
-
-    $scope.displayDate = function(){
-
-        $http({
-            url: '/getTime/'
-        })
-            .then(function(time){
-                $scope.dateData = time.data;
-                console.log($scope.dateData);
-                $scope.date = $scope.dateData[0];
-                $scope.time = $scope.dateData[1];
-            }
-        );
-    };
 
     $scope.displayMatrix = function(){
         $http({
             url: '/getReducedMatrix/'
         })
             .then(function(matrix){
-                $scope.matrix = matrix.data;
-                console.log($scope.matrix);
-            }
-        );
-    };
-
-    $scope.testDate = function(){
-        $scope.time ='Test time.';
-        console.log($scope.time);
-    };
-
-
-
-    $scope.findMolarMass = function(model){
-        if (model == null || model.name == null || model == "" || model.name == ""){
-            $scope.isError = true;
-            $scope.error = "You should fill it in!";
-        }
-
-        $scope.originalFormula = model;
-        console.log($scope.originalFormula);
-        if (!$scope.originalFormula){
-            $scope.error = "You should fill it in!";
-        }
-        $http({
-            url: '/postChemFormula',
-            method: "POST",
-            data: JSON.stringify({'formula':model}),
-            headers: {'Content-Type': 'application/json'}
-        })
-            .then(function(molarMass){
-                $scope.finalMolarMass = molarMass.data;
-                $scope.isError = null;
-                if ($scope.finalMolarMass == null){
-                    $scope.error = "Check your formula!";
-                    $scope.finalMolarMass = null;
-                }
-
-                console.log($scope.finalMolarMass);
-            },
-            function(molarMass){
-                console.log("Error in input.");
-                $scope.error = "Check your formula!";
-                $scope.finalMolarMass = null;
-                $scope.isError = true;
+              $scope.matrix = matrix.data;
+              //Delay output by a bit so that output is rendered properly.
+              setTimeout(function() {
+                MathJax.Hub.Queue(["Typeset",MathJax.Hub, "MatrixRREF"]);
+              });
+              console.log($scope.matrix);
             }
         );
     };
 
 });
-app.directive('mathjax',function(){
-	return {
-		restrict: 'EA',
-		link: function(scope, element, attrs) {
-			scope.$watch(attrs.ngModel, function () {
-				MathJax.Hub.Queue(['Typeset',MathJax.Hub,element.get(0)]);
-            });
-		}
-	};
-});
-
-function ExampleCtrl($scope){
-    $scope.changeModel = function(){
-         $scope.mjx = '\sum_{i=0}^\infty i^2';
-         console.log($scope.mjx);
-    }
-}
