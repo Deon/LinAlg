2c791bc59128d68333e28910965c715d0e2c9364
DeonHua <deonhbci@gmail.com>
2014-12-19 19:21:54 -0500

    Changed CSS and positioning of fields.

--- app.py
+++ app.py
@@ -31,4 +31,4 @@ def getMatrix():
     return response
 
 if __name__ == ("__main__"):
-    app.run(debug=True,host='0.0.0.0',port=int(os.environ.get("PORT", 5000)))
+    app.run(debug=True,host='0.0.0.0',port=int(os.environ.get("PORT", 5001)))
