f1ebac53a6dc3d46a83ff71654d3465e087f2e49
Deon Hua <dvhua@uwaterloo.ca>
2015-01-05 16:38:55 -0500

    Animations.

--- static/script.js
+++ static/script.js
@@ -24,4 +24,21 @@ app.config(['$routeProvider',
       otherwise({
         redirectTo: '/'
       })
-}]);
+}])
+.animation('.reveal-animation', function() {
+  return {
+    enter: function(element, done) {
+      element.css('display', 'none');
+      element.fadeIn(500, done);
+      return function() {
+        element.stop();
+      }
+    },
+    leave: function(element, done) {
+      element.fadeOut(500, done);
+      return function() {
+        element.stop();
+      }
+    }
+  }
+});
