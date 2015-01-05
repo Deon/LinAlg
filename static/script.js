782768d1f6d897ad5247ddd3cddd6f64adbeeaec
Deon Hua <dvhua@uwaterloo.ca>
2015-01-05 17:46:20 -0500

    Fixed animations.

--- static/script.js
+++ static/script.js
@@ -29,13 +29,13 @@ app.config(['$routeProvider',
   return {
     enter: function(element, done) {
       element.css('display', 'none');
-      element.fadeIn(500, done);
+      element.fadeIn(250, done);
       return function() {
         element.stop();
       }
     },
     leave: function(element, done) {
-      element.fadeOut(500, done);
+      element.fadeOut(250, done);
       return function() {
         element.stop();
       }
